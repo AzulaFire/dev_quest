326cbd63a190bff8c05e2865fa06a95a7ab6130c
AzulaFire <jhornjr@gmail.com>
2024-06-24 15:29:55 +0900

    Corrected Theme.tsx key issue

--- components/shared/navbar/Theme.tsx
+++ components/shared/navbar/Theme.tsx
@@ -5,7 +5,6 @@ import {
   MenubarContent,
   MenubarItem,
   MenubarMenu,
-  MenubarSeparator,
   MenubarTrigger,
 } from '@/components/ui/menubar';
 import Image from 'next/image';
@@ -39,34 +38,31 @@ const Theme = () => {
           </MenubarTrigger>
           <MenubarContent className='absolute -right-12 mt-3 min-w-[120px] rounded border py-2 dark:border-dark-400 dark:bg-dark-300'>
             {themes.map((theme) => (
-              <>
-                <MenubarItem
-                  key={theme.value}
-                  onClick={() => {
-                    setMode(theme.value);
-                    if (theme.value !== 'system') {
-                      localStorage.theme = theme.value;
-                    } else {
-                      localStorage.removeItem('theme');
-                    }
-                  }}
-                  className='flex items-center gap-4 px-2.5 py-2 dark:bg-dark-400'
+              <MenubarItem
+                key={theme.value}
+                onClick={() => {
+                  setMode(theme.value);
+                  if (theme.value !== 'system') {
+                    localStorage.theme = theme.value;
+                  } else {
+                    localStorage.removeItem('theme');
+                  }
+                }}
+                className='flex items-center gap-4 px-2.5 py-2 dark:bg-dark-400'
+              >
+                <Image
+                  src={theme.icon}
+                  alt='Icon'
+                  width={16}
+                  height={16}
+                  className={`${mode === theme.value && 'active-theme'}`}
+                />
+                <p
+                  className={`body-semibold text-light-500 ${mode === theme.value ? 'text-primary-500' : 'text-dark100_light900'}`}
                 >
-                  <Image
-                    src={theme.icon}
-                    alt='Icon'
-                    width={16}
-                    height={16}
-                    className={`${mode === theme.value && 'active-theme'}`}
-                  />
-                  <p
-                    className={`body-semibold text-light-500 ${mode === theme.value ? 'text-primary-500' : 'text-dark100_light900'}`}
-                  >
-                    {theme.label}
-                  </p>
-                </MenubarItem>
-                <MenubarSeparator />
-              </>
+                  {theme.label}
+                </p>
+              </MenubarItem>
             ))}
           </MenubarContent>
         </MenubarMenu>
